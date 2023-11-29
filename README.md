19861a14559910ee9d301ca1d6b6a5920af2cb36
ABDALLAH Karim <karim.abdallah@etu.univ-amu.fr>
2023-11-29 17:05:57 +0100

    Tri par selection

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 ## Algo de recherche 
 
 ### Recherche par dichotomie 
-    Cet algo doit etre fait dans un tableau trié et sa 
-    Complexité : de O( log n )
-    [Meilleure complexité d'ailleurs]
+    Cet algo doit etre fait dans un tableau trié.
+    Complexité :
+    Dans tous les cas ----> O( log n )  [Meilleure complexité d'ailleurs]
+
+
+## -------------------------------
+
+## Algo de tri de tableau
+
+### Tri par sélection 
+
+    Complexité :
+    O (n^2) car on a deux boucles dans l'algorithme de tri 
+    et il n y'a pas de meilleurs cas
+    
---
+++ triselection.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void triSelection(int Tab[], int n) {
+    int i, j, tmp;
+    int min;
+
+    printf("Avant le tri : \n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", Tab[i]);
+    }
+    printf("\n\n");
+
+    // Parcourir tous les éléments du tableau
+    for (i = 0; i <= n - 2; i++) {
+        min = i; // Trouver le min dans le tableau non trié
+
+        // Parcourir les éléments non triés
+        for (j = i + 1; j <= n; j++) {
+            // Si l'élement suivant est inf a au tab de min alors on attribue la case de la valeur au min
+            if (Tab[j] < Tab[min]) {
+                min = j;
+            }
+        }
+
+        // Échanger Tab[i] et Tab[min]
+        tmp = Tab[i];
+        Tab[i] = Tab[min];
+        Tab[min] = tmp;
+    }
+
+    printf("Apres le tri : \n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", Tab[i]);
+    }
+}
+
+int main() {
+    int n = 10;
+    int i;
+    int Tab[n];
+
+    // Utiliser time(NULL) comme graine pour la fonction rand()
+    srand(time(NULL));
+
+    for (i = 0; i < n; i++) {
+        Tab[i] = rand() % 100;  // Génère des nombres entre 0 et 99
+    }
+
+    triSelection(Tab, n);
+
+    return 0;
+}
